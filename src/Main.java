3a2c56b8af94cafe62250e071eda21aaf2c29551
Jeferson-Labout <jefson1989@gmai.com>
2023-02-26 00:03:53 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JavaScript");
+        curso2.setDescricao("descrição curso JavaScript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria do curso de java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
